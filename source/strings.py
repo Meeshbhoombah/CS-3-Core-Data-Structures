abf792de13217a4cc04e853a709c90269910f49e
Rohan Mishra <rohanmishra2017@gmail.com>
2018-01-21 21:39:09 -0800

    Finished palindromes

--- source/strings.py
+++ source/strings.py
@@ -5,6 +5,10 @@ def contains(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement contains here (iteratively and/or recursively)
+    if pattern in text:
+        return True
+    else:
+        return False
 
 
 def find_index(text, pattern):
