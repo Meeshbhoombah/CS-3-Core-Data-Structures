49dcd23bd16821838510f25b9e240778be816336
Rohan Mishra <rohanmishra2017@gmail.com>
2018-01-24 17:01:19 -0800

    Finished hashtables.py

--- source/hashtable.py
+++ source/hashtable.py
@@ -26,8 +26,8 @@ class HashTable(object):
     def load_factor(self):
         """Return the load factor, the ratio of number of entries to buckets.
         Best and worst case running time: ??? under what conditions? [TODO]"""
-        # TODO: Calculate load factor
-        # return ...
+        # calculate load factor
+        return self.size / self.buckets
 
     def keys(self):
         """Return a list of all keys in this hash table.
@@ -113,10 +113,10 @@ class HashTable(object):
             bucket.delete(entry)
         # Insert the new key-value entry into the bucket in either case
         bucket.append((key, value))
-        # TODO: Check if the load factor exceeds a threshold such as 0.75
-        # ...
-        # TODO: If so, automatically resize to reduce the load factor
-        # ...
+        # Check if the load factor exceeds a threshold such as 0.75
+        if self.load_factor() >= 0.75:
+            # If so, automatically resize to reduce the load factor
+            self._resize()
 
     def delete(self, key):
         """Delete the given key and its associated value, or raise KeyError.
@@ -139,19 +139,25 @@ class HashTable(object):
         such as 0.75 after an insertion (when set is called with a new key).
         Best and worst case running time: ??? under what conditions? [TODO]
         Best and worst case space usage: ??? what uses this memory? [TODO]"""
-        # If unspecified, choose new size dynamically based on current size
+        # If unspecified, choose new size dynamically 
+        # based on current size
         if new_size is None:
             new_size = len(self.buckets) * 2  # Double size
-        # Option to reduce size if buckets are sparsely filled (low load factor)
+            # new_size = self.find_prime(new_size) Double to nearest prime
         elif new_size is 0:
+            # Option to reduce size if buckets are sparsely 
+            # filled (low load factor)
             new_size = len(self.buckets) / 2  # Half size
+       
         # TODO: Get a list to temporarily hold all current key-value entries
-        # ...
+        all_items = self.items()
+        item_len = self.length()
         # TODO: Create a new list of new_size total empty linked list buckets
-        # ...
-        # TODO: Insert each key-value entry into the new list of buckets,
+        increment_buckets_by = new_size - item_len 
+        for
+        # TODO: Insert each key-value entry into the new list of buckets
+
         # which will rehash them into a new bucket index based on the new size
-        # ...
 
 
 def test_hash_table():
